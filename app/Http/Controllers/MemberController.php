ce13da4f2acba7ed065951858a48fa4e3cb5dfbb
Gamalielaradeya <Gamalielshroud@gmail.com>
2023-04-27 01:03:54 +0700

    view data from database into member

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Member;
 
 class MemberController extends Controller
 {
     //
     public function index()
     {
-        return view('member/index');
+        
+        return view('member/index', [
+            'members' => Member::get(),
+        ]);
     }
 }
--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -1,5 +1,36 @@
 @extends('template/default')
 
 @section('content')
-<h1>Member</h1>
+<div class="card">
+                  <div class="table-responsive">
+                    <table class="table table-vcenter card-table">
+                      <thead>
+                        <tr>
+                          <th>Nama</th>
+                          <th>Alamat</th>
+                          <th>No Telepon</th>
+                          <th>Tanggal Lahir</th>
+                          <th>Bulan Lahir</th>
+                          <th>Tahun Lahir</th>
+                          <th class="w-1"></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach ($members as $member)
+                        <tr>
+                          <td>{{ $member -> nama}}</td>
+                          <td>{{ $member -> alamat}}</td>
+                          <td>{{ $member -> phone_number}}</td>
+                          <td>{{ $member -> tanggal_lahir}}</td>
+                          <td>{{ $member -> bulan_lahir}}</td>
+                          <td>{{ $member -> tahun_lahir}}</td>
+                          <td>
+                            <a href="#">Edit</a>
+                          </td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
 @endsection
